85681e4eff4340c3fa40ee505dbe3a4bf879da1a
Jose María Dubra Valenzuela <josemariadubra@gmail.com>
2021-08-24 12:44:52 +0100

    Initial Commit

---
+++ Milkaway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milkaway
+//
+//  Created by Jose María Dubra Valenzuela on 24/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
